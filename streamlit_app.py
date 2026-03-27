e0d03ef33c935646da8a93055f29235cbbb1ba86
spsrin <palaniswamy.selvaraj@outlook.com>
2026-03-27 11:37:27 +0000

    Remove watermelon API call from streamlit_app.py
    Removed hardcoded API call for watermelon and its display.

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + ' Nutrition Information ')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
 
@@ -43,5 +46,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-st.text(smoothiefroot_response)
+
