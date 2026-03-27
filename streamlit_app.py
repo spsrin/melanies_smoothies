a6ed0b8459816625213c0cc8150f16ae82ecd699
spsrin <palaniswamy.selvaraj@outlook.com>
2026-03-27 11:59:28 +0000

    Refactor fruit selection and API call logic
    Updated dataframe handling and API request for fruit information.

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,10 +14,10 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 st.write("Choose the furits you want in your smoothie")
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
 #Convert Snowpark dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df = my_dataframe.to_pandas()
-st.stop
+#st.stop
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -34,11 +34,11 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-        search_on = pd_df.loc[pd_df['fruit_name'] == fruit_chosen, 'search_on'].iloc[0]
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
         
         st.subheader(fruit_chosen + ' Nutrition Information ')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{search_on}")  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
